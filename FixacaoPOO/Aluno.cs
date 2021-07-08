a2a1f3a13ec8461c1c152e1467a5c516127895fa
Livia <liviapereira1997@hotmail.com>
2021-07-07 21:03:15 -0300

    Add project files.

---
+++ FixacaoPOO/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FixacaoPOO
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public double DiferencaNotaFinal()
+        {
+            return 60 - NotaFinal();
+        }
+    }
+}
---
+++ FixacaoPOO/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixacaoPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e altura do retângulo:");
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = r.Area();
+            double perimetro = r.Perimetro();
+            double diagonal = r.Diagonal();
+
+            Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+
+            Funcionario f = new Funcionario();
+
+            Console.WriteLine("Nome: ");
+            f.Nome = Console.ReadLine();
+
+            Console.WriteLine("Salário Bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + f);
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f.AumentarSalario(porcentagem);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + f);
+            Console.WriteLine();
+
+            Aluno A = new Aluno();
+
+            Console.WriteLine("Nome do Aluno: ");
+            A.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as 3 notas do Aluno: ");
+            A.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            A.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            A.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double notafinal = A.NotaFinal();
+            double diferenca = A.DiferencaNotaFinal();
+
+            Console.WriteLine("Nota Final: " + notafinal.ToString("F2", CultureInfo.InvariantCulture));
+
+            if(notafinal >= 60)
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO, FALTARAM " + diferenca.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            
+
+        }
+
+    }
+}
